a5d79ec9af8f05b9affb715bde2d61e21c69dda1
sujith-penodia <sujiththaiparambil@gmail.com>
2021-03-01 22:58:38 +0530

    March 1

--- functions.php
+++ functions.php
@@ -1,10 +1,27 @@
 <?php 
-	 add_action( 'wp_enqueue_scripts', 'brindle_tierra_enqueue_styles' );
-	 function brindle_tierra_enqueue_styles() {
- 		  wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' ); 
- 		  } 
+// Starts the engine.
+//require_once get_template_directory() . '/inc/customizer.php';
 
- 		  require 'vendor/plugin-update-checker/plugin-update-checker.php';
+add_action( 'wp_enqueue_scripts', 'brindle_tierra_enqueue_styles' );
+function brindle_tierra_enqueue_styles() {
+wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' ); 
+} 
+
+
+
+// Includes color stuff
+require_once get_stylesheet_directory() . '/inc/defaults.php';
+require_once get_stylesheet_directory() . '/inc/customizer.php';
+require_once get_stylesheet_directory() . '/inc/css-output.php';
+
+
+
+
+
+
+
+
+require 'vendor/plugin-update-checker/plugin-update-checker.php';
 $myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
 	'https://github.com/sujith-penodia/luxury-child-theme/',
 	__FILE__, //Full path to the main plugin file or functions.php.
